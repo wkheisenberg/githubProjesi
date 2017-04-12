2d50af8b6b8d32a8c5dc7294df6afd0336cdb56e
Mansur Akbas <mansur.akbas@outlook.com>
2017-04-12 21:27:08 +0300

    recursiveFunction
    recursiveFunction sample

---
+++ githubProjesi/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+using System;
+
+namespace githubProjesi
+{
+    class Program
+    {
+        static int Fact(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * Fact(n - 1);
+        }
+
+        static void Main()
+        {
+            while (true)
+            {
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Fact(sayi));
+            }
+        }
+    }
+}
